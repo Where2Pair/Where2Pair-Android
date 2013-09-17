c67dd983c7d2b4c64f54dace9f9c35fe7523f726
Robert Taylor <robert.taylor426@gmail.com>
2013-09-17 08:41:08 +0100

    Adding VenuesResultActionTest

--- src/org/where2pair/VenuesResultAction.java
+++ src/org/where2pair/VenuesResultAction.java
@@ -1,11 +1,17 @@
 package org.where2pair;
 
+import java.util.List;
+
 public class VenuesResultAction {
 
 	private final VenuesResultActionHandler venuesResultActionHandler;
 
 	public VenuesResultAction(VenuesResultActionHandler venuesResultActionHandler) {
 		this.venuesResultActionHandler = venuesResultActionHandler;
+	}
+
+	public void apply(List<Venue> venues) {
+		venuesResultActionHandler.notifyVenuesFound(venues);
 	}
 	
 }
---
+++ test/org/where2pair/VenuesResultActionTest.java
@@ -0,0 +1,28 @@
+package org.where2pair;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VenuesResultActionTest {
+
+	@Mock VenuesResultActionHandler venuesResultActionHandler;
+	
+	@Test
+	public void notifiesActionHandlerWhenVenuesSet() {
+		List<Venue> venues = newArrayList(new Venue(), new Venue());
+		VenuesResultAction venuesResultAction = new VenuesResultAction(venuesResultActionHandler);
+		
+		venuesResultAction.apply(venues);
+		
+		verify(venuesResultActionHandler).notifyVenuesFound(venues);
+	}
+	
+}
